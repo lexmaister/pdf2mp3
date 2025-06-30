70548ececa93827ca70990b65c9e68ce055ad3a9
Alex Aleev <aleev@simicon.com>
2025-06-30 13:34:17 +0300

    chore: cleanup repository

--- build/lib/pdf2mp3/__init__.py
+++
@@ -1,7 +0,0 @@
-# pdf2mp3 package
-
-__version__ = "0.1.0"
-
-# Expose main functionality here if desired for library use, e.g.:
-# from .core import convert_pdf_to_mp3
-# from .core import list_kokoro_languages, list_kokoro_voices
